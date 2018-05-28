a7c23442d6adbebb34caa31cc012d47cb01c9b3a
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-05-28 18:15:02 -0400

    Autocommit por Git-Cron: alterações: add 'oc-cli.sh'

--- oc-cli.sh
+++ oc-cli.sh
@@ -7,6 +7,8 @@
 # Criado em: 2018-05-24 15:47:04
 # Última alteração: 2018-05-28 17:51:01
 
+sep="---------------------------------------------"
+
 banner() {
 	echo
 	echo "                      _ _ "
@@ -24,8 +26,6 @@ ajuda() {
 	exit 0
 }
 
-
-
 #if [ "$(mysql -uUSER -pPASS -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$usuario')")" = 1 ]; then
 #	echo "O usuário $usuario existe, abortando."
 #	exit 1
@@ -38,3 +38,31 @@ ajuda() {
 #unzip /tmp/opencart/opencart-$ultima.zip
 
 banner
+
+read -p "Nome do site? [Padrão: nenhum]: " nome
+[ -z $nome ] && echo "O site precisa de um nome. Abortando..." && exit 1
+
+read -p "Em qual pasta o Opencart deve ser instalado? [Padrão: $(pwd)]: " pasta
+pasta=${pasta:-"$(pwd)"}
+
+#read -p "Em qual pasta o Opencart deve ser instalado? [Padrão: $(pwd)]: " pasta
+
+#read -p "Em qual pasta o Opencart deve ser instalado? [Padrão: $(pwd)]: " pasta
+
+#read -p "Em qual pasta o Opencart deve ser instalado? [Padrão: $(pwd)]: " pasta
+#pasta=${pasta:-$(pwd)}
+
+#pasta=${pasta:-$(pwd)}
+
+echo $sep
+banner
+echo $sep
+echo "DADOS DO NOVO SITE"
+echo $sep
+echo "Site:    $nome"
+echo "Usuario: $usuario_db"
+echo "Senha:   $senha_db"
+echo "Pasta:   $pasta"
+echo $sep
+
+
