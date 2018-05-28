abdc1a39359e57b2cc79be659bbf80ccab6934ac
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-05-28 17:55:36 -0400

    Alterações no programa

--- oc-cli.sh
+++ oc-cli.sh
@@ -5,7 +5,17 @@
 # Feito por Lucas Saliés Brum, lucas@archlinux.com.br
 #
 # Criado em: 2018-05-24 15:47:04
-# Última alteração: 2018-05-24 16:50:09
+# Última alteração: 2018-05-28 17:51:01
+
+banner() {
+	echo
+	echo -n "                      _ _ "
+	echo -n "  ___   ___       ___| (_)"
+	echo -n " / _ \ / __|____ / __| | |"
+	echo -n "| (_) | (_|_____| (__| | |"
+	echo -n " \___/ \___|     \___|_|_|"
+	echo
+}
 
 ajuda() {
 	echo
@@ -13,6 +23,8 @@ ajuda() {
 	echo
 	exit 0
 }
+
+
 
 #if [ "$(mysql -uUSER -pPASS -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$usuario')")" = 1 ]; then
 #	echo "O usuário $usuario existe, abortando."
@@ -25,17 +37,4 @@ ajuda() {
 #curl -s -o /tmp/opencart/opencart-$ultima.zip $(curl -s "https://api.github.com/repos/opencart/opencart/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
 #unzip /tmp/opencart/opencart-$ultima.zip
 
-for a in $@; do
-    pa=$(echo $a | awk -F= '{print $1}')
-    va=$(echo $a | awk -F= '{print $2}')
-
-	case $pa in
-        -u | --usuario) usuario=$va ;;
-		-s | --senha) senha=$va ;;
-		-b | --banco) banco=$va ;;
-		-c | --caminho) caminho=$va ;;
-        *) ajuda ;;
-    esac
-done
-
-echo "$banco $senha $usuario"
+banner
